40f8cdd832a16a2cc51e93b65d8c73661074a809
steve.adams <sabond.tx@gmail.com>
2019-09-14 17:30:01 -0500

    test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VueDraggableResizable from "vue-draggable-resizable";
-import "vue-draggable-resizable/dist/VueDraggableResizable.css";
+import Vue from 'vue';
+import App from './App.vue';
+import VueDraggableResizable from 'vue-draggable-resizable';
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css';
 
-Vue.component("vue-draggable-resizable", VueDraggableResizable);
 Vue.config.productionTip = false;
+Vue.component( 'vue-draggable-resizable', VueDraggableResizable );
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+new Vue({ render: h => h( App )}).$mount( '#app' );
